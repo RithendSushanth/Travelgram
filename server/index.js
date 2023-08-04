6e02d3ecedb4e273af33df4b9e4ae86faa0f0bea
Rithend <rithend21@gmail.com>
2023-08-04 20:20:43 +0530

    Initial Commit , CRUD DONE

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+const PORT = 5000;
+
+// Code with hidden links are in notepad
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+const MONGO_URI = "mongodb+srv://rithend21:tcs2324065@cluster0.hdphopa.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB Atlas:', error.message);
+  });
